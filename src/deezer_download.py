c3c3a201974ba6d82138003caf390ce4fbaa2e98
jbh-cloud <72975077+jbh-cloud@users.noreply.github.com>
2020-12-16 21:05:22 +0000

    Final: rewrote to ensure doesnt try to redownload same songs

---
+++ src/deezer_download.py
@@ -0,0 +1,9 @@
+from deemix.__main__ import download
+
+#download(url, bitrate, portable, path)
+
+download('https://www.deezer.com/track/1160380192', '320', '--portable', 'D:\\deemix_music')
+
+
+
+
