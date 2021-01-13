28c06527e87c5b04b7a5c0336053d4b8885de02c
jbh-cloud <72975077+jbh-cloud@users.noreply.github.com>
2021-01-13 16:12:45 +0000

    Major refactor

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import src.generateLikedSongsJson as downloadSpotifyLikedSongs
 import src.processLikedSongs as processLikedSongs
 import src.exportDeezerLinksFromProcessedSongs as exportDeezerLinks
+import src.spotify_api as spotify_api
+import src.transform as transform
+import src.banner as banner
+import src.download as download
 
-liked_songs_path = 'data/spotify_liked_songs.json'
-processed_songs_path = 'data/processed_songs.json'
-deezer_url_path = 'data/deezer_urls.txt'
+def main():
+    banner.script_start()
 
-print('Spotify liked song downloader v1')
+    # Download latest Spotify likes
+    spotify_api.download_liked()
 
-# Download latest Spotify likes
-downloadSpotifyLikedSongs.run(liked_songs_path)
+    # Check for new (unprocessed) liked songs and attempt to match to Deezer, save to processed_songs
+    transform.process_liked()
 
-# Check for new (unprocessed) liked songs and attempt to match to Deezer
-processLikedSongs.run(liked_songs_path, processed_songs_path)
+    # Download pending songs
+    download.missing_tracks()
 
-# Generate Deezer file for any new songs
-exportDeezerLinks.run(processed_songs_path, deezer_url_path)
+    # Generate Deezer file for any new songs
+    #exportDeezerLinks.run(processed_songs_path, deezer_url_path)
+
+
+if __name__ == '__main__':
+    main()
+
