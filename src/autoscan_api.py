e32bb430cd9a88abb57888207428a07f9a73b55f
JBH <admin@jbh.cloud>
2021-01-24 14:45:04 +0000

    Fixing post request

--- src/autoscan_api.py
+++ src/autoscan_api.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+import urllib
 
 # local imports
 import src.config as config
@@ -13,21 +14,23 @@ def scan(paths):
         for p in paths:
             if config['autoscan']['scan_file_parent_path']:
                 p = Path(p).parent
-                #print(f'scan_file_parent_path is true, scanning parent: {p}')
 
-            data = {
+            params = {
                 'dir': p
             }
 
             if config['autoscan']['auth_enabled']:
-                response = requests.post(config['autoscan']['endpoint'], data=data, auth=(
+                response = requests.post(config['autoscan']['endpoint'], params=params, auth=(
                     config['autoscan']['username'],
                     config['autoscan']['password'])
                 )
             else:
-                response = requests.post(config['autoscan']['endpoint'], data=data)
+                response = requests.post(config['autoscan']['endpoint'], params=params)
 
             if response.status_code == 200:
-                print(f'PLex scan request: {p}')
+                print(f'Plex scan request: {p}')
             else:
                 print(f'Failed to send Plex scan notification: {p}')
+
+
+scan(['/mnt/unionfs/Media/Spotify/liked/CoryaYo/Late Bloom'])
